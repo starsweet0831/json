154c99a2fb88edb088b1b4df8243bb40f4166504
starsweet0831 <star_sweet25@yahoo.com.tw>
2021-03-22 21:11:17 +0800

    新增資料

---
+++ main.js
@@ -0,0 +1,30 @@
+var link = "https://datacenter.taichung.gov.tw/Swagger/OpenData/2072c001-c06e-4276-bbf1-84f3b113c38c?limit=1000";
+
+const getData = {
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("GET", link);
+
+        xhr.send();
+
+        xhr.onload = () => {
+            var json = xhr.responseText;
+            var obj = JSON.parse(json);
+
+            console.log(obj);
+        }
+
+    }
+};
+
+Vue.createApp(getData).mount("#data");
+
+console.log
+
